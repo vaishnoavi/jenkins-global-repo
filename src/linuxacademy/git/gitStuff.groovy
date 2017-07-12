993279c8658973eb1b61f33be7623295ea5af136
Ashish <abc.vaishno@ashish.com>
2017-07-12 18:00:26 +0000

    speeling check

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,5 +1,5 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	return "/bin/git --git-dir=${gitPath} rev-parse HEAD".execute().text"
+	return "/bin/git --git-dir=${gitPath} rev-parse HEAD.execute().text"
 }
